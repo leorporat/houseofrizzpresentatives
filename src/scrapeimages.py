79e32b1457c152b8fbf6ba7d277bbca724b0c2b7
mulupuruvikas <vikas.mulupuru@gmail.com>
2023-01-28 23:55:20 -0600

    successfully scraping the names and urls duplicating the names and image urls, no gender information urls are formatted incorrectly as well

--- src/scrapeimages.py
+++ src/scrapeimages.py
@@ -1,6 +1,11 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
+filenames = open('congresspeoplenames.txt', 'w')
+fileimages = open('congresspeopleimages.txt', 'w')
+
+##First webpage
 with open("congressmembers1.html") as file:
 	soup = BeautifulSoup(file, 'html.parser')
 
@@ -10,7 +15,45 @@ for image in images:
 	image_url = image.get("src")
 	image_name = image.get("alt")
 
-	print(image_name)
-	print(image_url)
+	if image_url[-3:] == 'jpg':
+		filenames.write(image_name + '\n')
+		fileimages.write(image_url + '\n')
+file.close()
+
+
+##Second webpage
+with open("congressmembers2.html") as file:
+	soup = BeautifulSoup(file, 'html.parser')
+
+images = soup.find_all("img")
+
+for image in images:
+	image_url = image.get("src")
+	image_name = image.get("alt")
+
+	if image_url[-3:] == 'jpg':
+		filenames.write(image_name + '\n')
+		fileimages.write(image_url + '\n')
+file.close()
+
+##Third webpage
+with open("congressmembers3.html") as file:
+	soup = BeautifulSoup(file, 'html.parser')
+
+images = soup.find_all("img")
+
+for image in images:
+	image_url = image.get("src")
+	image_name = image.get("alt")
+	if image_url[-3:] == 'jpg':
+		filenames.write(image_name + '\n')
+		fileimages.write(image_url + '\n')
+file.close()
+filenames.close()
+fileimages.close()
+
+
+	
+	
 
 #print(soup.prettify())
