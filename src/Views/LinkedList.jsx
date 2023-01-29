0ac40226bfb1d9e016e38470d4e488f0dfbff03b
mulupuruvikas <vikas.mulupuru@gmail.com>
2023-01-29 02:05:53 -0600

    finished add in linked list

--- src/Views/LinkedList.jsx
+++ src/Views/LinkedList.jsx
@@ -2,4 +2,20 @@ class LinkedList {
     constructor(head = null) {
         this.head = head
     }
+
+    add(name, location) {
+        var addNode = new ListNode(name, location);
+        var current;
+        if (this.head == null)
+            this.head = node;
+        else {
+            current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+    }
+
+
 }
