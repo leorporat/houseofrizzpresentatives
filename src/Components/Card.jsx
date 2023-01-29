cdb39b59644a3777c9f66dba6d45f021c5fda15f
Leor Porat <lporat874@student.fuhsd.org>
2023-01-29 01:34:23 -0600

    push for sync

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,7 +2,7 @@ function Card({image, name}) {
     return (
         <div className='card-container'>
             <div className='card-image-container'>
-                <img className='card-image'>{image}</img>
+                <img className='card-image' alt='N/A' src={image}/>
             </div>
             <div className='card-name-container'>
                 <p className='card-name'>{name}</p>
--- src/Views/VoteGuysView.jsx
+++ src/Views/VoteGuysView.jsx
@@ -1,6 +1,17 @@
+import Card from '../Components/Card.jsx'
+import a000055_200 from '../congressmembers1_files/a000055_200.jpg'
+import a000148_200 from '../congressmembers1_files/a000148_200.jpg'
+
 function VoteGuysView() {
     return (
-        
+        <div className='guys-selections'>
+            <Card
+            image={a000055_200}
+            name='Fuckface Mcgee'/>
+            <Card
+            image={a000148_200}
+            name='Other Fuckface' />
+        </div>
     )
 }
 
