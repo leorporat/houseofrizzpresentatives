32440872a6e9c29b578a700f2d21f91c0a8634a6
mulupuruvikas <vikas.mulupuru@gmail.com>
2023-01-29 02:06:46 -0600

    finished remove in linked list

--- src/Views/LinkedList.jsx
+++ src/Views/LinkedList.jsx
@@ -17,5 +17,25 @@ class LinkedList {
         }
     }
 
+    remove(position) {
+        var curr, prev, it = 0;
+        curr = this.head;
+        prev = curr;
 
+        // deleting first element
+        if (index === 0) {
+            this.head = curr.next;
+        } else {
+            // iterate over the list to the
+            // position to removce an element
+            while (it < index) {
+                it++;
+                prev = curr;
+                curr = curr.next;
+            }
+
+            // remove the element
+            prev.next = curr.next;
+        }
+    }
 }
