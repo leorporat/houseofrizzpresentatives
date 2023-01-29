9768b02dbd4caa376965322787ab662e0786086e
mulupuruvikas <vikas.mulupuru@gmail.com>
2023-01-29 05:37:42 -0600

    made the print of the names and images into an array format

--- src/sortingcongress.py
+++ src/sortingcongress.py
@@ -3,18 +3,25 @@ fileURLs = open('congresspeopleimages.txt', 'r')
 namelines = fileNames.readlines()
 imagelines = fileURLs.readlines()
 
-maleNames = open('./Views/congressmalenames.txt', 'a')
-femaleNames = open('./Views/congressfemalenames.txt', 'a')
+maleNames = open('./Views/congressmalenames.txt', 'w')
+femaleNames = open('./Views/congressfemalenames.txt', 'w')
 maleURLs = open('./Views/congressmaleimages.txt', 'w')
 femaleURLs = open('./Views/congressfemaleimages.txt', 'w')
-
+maleNames.write('[')
+maleURLs.write('[')
+femaleNames.write('[')
+femaleURLs.write('[')
 for i in range(len(namelines)):
 	if namelines[i][-2] == 'M':
-		maleNames.write(namelines[i][0:-2] + '\n')
-		maleURLs.write(imagelines[i])
+		maleNames.write('\'' + namelines[i][0:-2] + '\', ')
+		maleURLs.write('\'' + imagelines[i][:-1] + '\', ')
 	elif namelines[i][-2] == 'F':
-		femaleNames.write(namelines[i][0:-2] + '\n')
-		femaleURLs.write(imagelines[i])
+		femaleNames.write('\'' + namelines[i][0:-2] + '\', ')
+		femaleURLs.write('\'' + imagelines[i][:-1] + '\', ')
+maleNames.write(']')
+maleURLs.write(']')
+femaleNames.write(']')
+femaleURLs.write(']')
 
 fileNames.close()
 fileURLs.close()
