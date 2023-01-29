6d9123d0c982eb0972fc6e8fdc2a06eb6eff3aef
mulupuruvikas <vikas.mulupuru@gmail.com>
2023-01-28 22:30:19 -0600

    first attempt: retrieving images from file

--- src/scrapeimages.py
+++ src/scrapeimages.py
@@ -1,4 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-with open("congr")
+with open("congressmember.html") as file:
+	soup = BeautifulSoup(file, 'html.parser')
+
+images = soup.find_all("img")
+
+for image in images:
+	image_url = image.get("src")
+	image_name = image.get("alt")
+
+	response = requests.get(image_url)
+	with open(image_url.split("/")[-1], "wb") as f:
+        f.write(response.content)
+
+print(soup.prettify())
